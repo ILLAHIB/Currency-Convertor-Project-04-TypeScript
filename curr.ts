ade7b1ff06714eb750739f7f84154f49bea87fd9
ILLAHIB <155870604+ILLAHIB@users.noreply.github.com>
2024-01-18 14:45:09 +0500

    Add files via upload

---
+++ curr.ts
@@ -0,0 +1,70 @@
+
+import inquirer from "inquirer";
+
+
+let loop = true;
+interface UserInput{
+    currencyInput: string,
+    amountInput: number,
+    currencyOutput: string
+}
+
+while(loop){
+
+    const userInput: UserInput = await inquirer.prompt([
+        {
+        type: "list",
+        name: "currencyInput",
+        choices: ["Pakistani Rupee", "US Dollar", "Saudi Riyal"],
+        message: "Select your input Currency"
+        },
+        {
+            type: "number",
+            name: "amountInput",
+            message: "Enter the amount you want to convert"
+        },
+        {
+            type: "list",
+            name: "currencyOutput",
+            choices: ["Pakistani Rupee", "US Dollar", "Saudi Riyal"],
+            message: "Select your output Currency"
+        }
+]);
+
+const input = userInput.currencyInput;
+const output = userInput.currencyOutput;
+const amount = userInput.amountInput;
+
+if(input === "Pakistani Rupee"){
+    if(output === "US Dollar"){
+        console.log(`The ${amount} Pakistan Rupee is equal to ${amount / 281} US Dollar`)
+    }else if(output === "Saudi Riyal"){
+        console.log(`The ${amount} Pakistan Rupee is equal to ${amount / 75} Saudi Riyal`)
+    }
+
+}else if(input === "US Dollar"){
+    if(output === "Pakistani Rupee"){
+        console.log(`The ${amount} US Dollar is equal to ${amount * 281} Pakistan Rupee`)
+    }else if(output === "Saudi Riyal")
+    console.log(`The ${amount} US Dollar is equal to ${amount * 3.75} Saudi Riyal`)
+
+
+}else if(input === "Saudi Riyal"){
+    if(output === "Pakistani Rupee"){
+        console.log(`The ${amount} Saudi Riyal is equal to ${amount * 75} Pakistan Rupee`)
+    }else if(output === "US Dollar")
+    console.log(`The ${amount} Saudi Riyal is equal to ${amount / 3.75} US Dollar`)
+}
+
+const calulateAgain = await inquirer.prompt({
+    type: "confirm",
+    name: "calulateMore",
+    message: "Do you want more conversions?",
+    default: false
+});
+
+if(!calulateAgain.calulateMore){
+    loop = false;
+}
+
+}
